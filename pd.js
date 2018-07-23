3eb4b2cc9091bd2d2265a03a19b9158002ab729b
Z Yin <Z.Yin.MD@gmail.com>
2018-07-23 15:30:28 -0400

    add recursive scan sub folders

--- pd.js
+++ pd.js
@@ -8,6 +8,13 @@ const path = require('path');
 
 printHeader();
 
+var DIR = ''; // a global variable to store the folder that user wants to perform phantom delete on
+
+var CLI = readline.createInterface({ // got this part from node docs - readline
+    input: process.stdin,
+    output: process.stdout
+  });
+
 class File { // a case of File is an actual file, to construct, pass in its path and its name
   constructor(dir, name) {
     this.dir = dir;
@@ -15,6 +22,9 @@ class File { // a case of File is an actual file, to construct, pass in its path
   }
   get fullname() {
     return path.join(this.dir, this.name);
+  }
+  get partialName() {
+    return '.' + this.fullname.replace(DIR, '');
   }
   get size() {
     return fs.statSync(this.fullname).size;
@@ -34,7 +44,7 @@ class File { // a case of File is an actual file, to construct, pass in its path
   }
   get displayInfo() {
     var space = 7 - this.readableSize.length;
-    return ' '.repeat(space) + this.readableSize + '  '+ this.name ;
+    return ' '.repeat(space) + this.readableSize + '  '+ this.partialName;
   }
   isFolder() {
     return fs.statSync(this.fullname).isDirectory();
@@ -44,9 +54,12 @@ class File { // a case of File is an actual file, to construct, pass in its path
       if (err) throw err;
     });
   }
+  prefix() {
+    fs.renameSync(this.fullname, path.join(this.dir, '⨯ ' + this.name));
+  }
 }
 
-class BigFileList { // a case of BigFileList is an array of big files (bigger than the threshold to be deleted), to construct, pass in array of Files
+class ToBeDeleted { // a case of ToBeDeleted is an array of Files bigger than the threshold to be deleted, to construct, pass in array of Files
   constructor(fileList) {
     this.fileList = fileList;
   }
@@ -65,34 +78,52 @@ class BigFileList { // a case of BigFileList is an array of big files (bigger th
   deleteAll() {
     for (let i of this.fileList)
       i.delete();
-    console.log(`\n- ${this.length} files phantom-deleted.`);
+    console.log(`\n  ...${this.length} files phantom-deleted.`);
+  }
+  prefixAll() {
+    for (let i of this.fileList)
+      i.prefix();
   }
 }
-
-var CLI = readline.createInterface({ // got this part from node docs - readline
-    input: process.stdin,
-    output: process.stdout
-  });
 
 run();
 
 async function run() {
-  try {
-    var dir = await question('Which folder do you want to perform phantom-delete on? Drag the folder into here:\n');
-    dir = dir.trim(); // in some terminals, dragging creates space in the end
-    if (dir[0] == dir.slice(-1) && (dir[0] == "'" || dir[0] == '"')) // when there's space in the path, some terminals auto add quotes, resulting in strings with redundant quotes
-      dir = dir.slice(1, -1);
-    var bigFiles = new BigFileList([]); // the object that stores the to-be-deleted files
+
+  function scanDir(dir) { // scan a folder for big files
     var fileList = fs.readdirSync(dir);
     for (let filename of fileList) {
       let file = new File(dir, filename);
-      if (file.size < threashold || file.isFolder()) continue;
-      bigFiles.fileList.push(file);
+      if (file.isFolder()) {
+        if (recursive == 'y') {
+          folders.push(file);
+          scanDir(file.fullname);
+        } else continue;
+      }
+      else if (file.size < threashold) continue;
+      else bigFiles.push(file);
     }
-    console.log(`\n- ${fileList.length} files found, ${bigFiles.length} of which are big and will be phantom-deleted. \n- This can free up ${File.toReadableSize(bigFiles.totalSize)} disk space.`);
+  }
 
-    var wantSeeFiles = await question(`\n- (default = n) Would you like to view the list of them? (y / n):`);
-    if (wantSeeFiles.toLowerCase().trim() == 'y')
+  try {
+    DIR = await question('Which folder do you want to perform phantom-delete on? Drag the folder into here:\n');
+    DIR = DIR.trim(); // in some terminals, dragging creates space in the end
+    if (DIR[0] == DIR.slice(-1) && (DIR[0] == "'" || DIR[0] == '"')) // when there's space in the path, some terminals auto add quotes, resulting in strings with redundant quotes
+      DIR = DIR.slice(1, -1);
+
+    var recursive = await question(`\n- (default = n) Include sub-folders? (y / n):`);
+    recursive = recursive.toLowerCase().trim();
+
+    var bigFiles = []; // to store an array of files bigger than threshold.
+    var folders = []; // to store an array of folders and sub-folders found
+
+    scanDir(DIR);
+
+    bigFiles = new ToBeDeleted(bigFiles);
+    console.log(`\n  ...Found ${bigFiles.length} files bigger than 1MB.\n\n- Phantom-delete them will free up ${File.toReadableSize(bigFiles.totalSize)} disk space.`);
+
+    var viewBeforeDelete = await question(`\n- (default = n) Would you like to view a list of them? (y / n):`);
+    if (viewBeforeDelete.toLowerCase().trim() == 'y')
       bigFiles.showFiles();
 
     var confirm = await question('\n- Type "confirm" to proceed (cannot undo):');
@@ -102,18 +133,26 @@ async function run() {
     }
     bigFiles.deleteAll();
 
-    var wantPrefix = await question(`\n- (default = y) Prefix your folder name with a "╳"? (y / n):`);
-    if (wantPrefix.toLowerCase().trim() != 'n') {
-      let newBasename = '⨯' + path.basename(dir); // ⨯ is the real char used, but it doesn't show well in some terminals. ╳ shows better.
-      fs.renameSync(dir, path.join(path.dirname(dir), newBasename));
-      console.log(`\n- Your folder name has been changed to "${'╳' + newBasename.slice(1, newBasename.length)}"`);
+    var prefixFiles = await question(`\n- (default = y) Prefix phantom file names with a "╳"? (y / n):`);
+    if (prefixFiles.toLowerCase().trim() != 'n') {
+      bigFiles.prefixAll();
+      console.log(`\n  ...phantom file names changed.`);
+    }
+
+    var prefixFolders = await question(`\n- (default = n) Prefix your folder name(s) with a "╳"? (y / n):`);
+    if (prefixFolders.toLowerCase().trim() == 'y') {
+      folders.reverse(); // rename the deepest folders first
+      folders.push(new File(path.dirname(DIR), path.basename(DIR))); //DIR last
+      for (let i of folders)
+        i.prefix();
+      console.log(`\n  ...folder names changed.`);
     }
     CLI.close();
   }
 
   catch (error) {
     if (error.message.startsWith('EPERM') || error.message.startsWith('EACCES'))
-      console.log('\nPhantom-delete successful, but failed to prefix your folder with a "╳"');
+      console.log("\nError occured:\nPhantom-delete was successful, but failed in prefixing a certain file or folder with ╳. Maybe it's in use. Here's the error message: \n\n", error);
     else if (error.message.startsWith('ENOENT'))
       console.log('\nUnrecognized Path. Are you using WSL? If yes, try cmd, or Git Bash, or Powershell, they all work.');
     else {
@@ -122,7 +161,9 @@ async function run() {
     }
     process.exit();
   }
+
 }
+
 
 function question(question) {
   return new Promise(resolve => {
